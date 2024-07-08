aa4c23ca7bf28c439c6da435066e9d905f91649a
Davis Smith <44070645+davissmith1@users.noreply.github.com>
2024-07-08 16:55:12 -0400

    cosmectic updates

--- main.sh
+++ main.sh
@@ -2,9 +2,27 @@
 
 #Main binary thaht creates project 
 echo "$(curl -s https://raw.githubusercontent.com/BucknerHeavyLiftCranes/apiGen/main/utils/apiWizard.txt)"
-echo "Please enter the name of the project: "
 
-read projectName
+while true; do
+    echo "Please enter the name of the project: "
+    read projectName
+    if [[ "$projectName" =~ ^[a-zA-Z0-9_-]+$ ]]; then
+        if [ -d "$HOME/Projects/$projectName" ]; then
+            echo "Directory with the name $projectName already exists. Do you want to overwrite it? (y/n): "
+            read overwrite
+            if [[ "$overwrite" == "y" || "$overwrite" == "Y" ]]; then
+                rm -rf $HOME/Projects/$projectName
+                break
+            else
+                echo "Please enter a different project name."
+            fi
+        else
+            break
+        fi
+    else
+        echo "Invalid project name. Please use only letters, numbers, hyphens, and underscores."
+    fi
+done
 
 echo "Creating project $projectName"
 
@@ -24,7 +42,20 @@ echo "$projectName is a project created by the Buckner Heavy Lift Cranes API tea
 curl https://raw.githubusercontent.com/BucknerHeavyLiftCranes/apiGen/main/endpoints.sh > endpoints.sh
 chmod +x endpoints.sh
 
-echo "running endpoint creation script"
+echo "Running endpoint creation script \n"
+echo -ne 'Loading: [                    ] (0%)\r'
+sleep 0.2
+echo -ne 'Loading: [#####               ] (25%)\r'
+sleep 0.2
+echo -ne 'Loading: [##########          ] (50%)\r'
+sleep 0.2
+echo -ne 'Loading: [###############     ] (75%)\r'
+sleep 0.2
+echo -ne 'Loading: [####################] (100%)\r'
+echo -ne '\n'
+
+
+clear
 ./endpoints.sh
 
 echo ".gitignore" >> .gitignore
